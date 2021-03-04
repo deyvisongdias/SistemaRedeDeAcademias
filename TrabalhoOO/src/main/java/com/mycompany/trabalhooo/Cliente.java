de93c46d6288ff251e547c72f25d063ae5d0ab84
Yuri-1525 <63771905+Yuri-1525@users.noreply.github.com>
2021-03-04 16:59:33 -0300

    Update Cliente.java

--- TrabalhoOO/src/main/java/com/mycompany/trabalhooo/Cliente.java
+++ TrabalhoOO/src/main/java/com/mycompany/trabalhooo/Cliente.java
@@ -6,8 +6,6 @@ Yuri de Oliveira (201835010)*/
 
 package com.mycompany.trabalhooo;
 
-import java.util.List;
-import java.util.ArrayList;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -18,118 +16,117 @@ import java.text.SimpleDateFormat;
 
 public class Cliente {
 
-    static int contId = 0; // Vai ser usada pra achar a ficha
-    static int contMatriculas = 0; //  Ativas
+    static int contId; // Vai ser usada pra achar a ficha
+
 
     private final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-    private int id;
+    private String tipoPlano;
     private String nome;
-    private int cpf;
+    private String cpf;
     private Date dataMatricula;
-    private Boolean statusMatricula;
     private Date vencimento;
-    private String senha;
-    private double mensalidade;
-    private List <Ficha> ficha = new ArrayList<>(); //Necessario colocar isso?
-
+    private int id;
+    private Boolean statusMatricula;
+//  private Ficha ficha;
+    private double valorPlano;
+    private int acrescimo = 1;
     
-    //!!!! Colocar Ficha depois !!!!
     //Construtor de um cliente novo
-    public Cliente(String nome,int cpf, Date dataMatricula,String senha) {
+    public Cliente(String tipoPlano,String nome,String cpf, Date dataMatricula) {
         contId++;
         Calendar cal = Calendar.getInstance();
         cal.setTime(dataMatricula);
         cal.add(Calendar.MONTH, 1);
+        this.tipoPlano = tipoPlano;
         this.nome = nome;
         this.cpf = cpf;  
         this.dataMatricula = dataMatricula; 
         vencimento = cal.getTime();
         this.id = contId;
         this.statusMatricula = true;
-        this.senha = senha;
-        mensalidade = 80.0;
+        valorPlano = 80;
     }
     
-    //Construtor de um cliente que já existe, com o construtor de cima, dados como vencimento e statusMatricula
+    //Construtor de um cliente já cadastrado, com o construtor de cima, dados como vencimento e statusMatricula
     //poderiam se perder. Usado quando lê os dados de um arquivo
     
-    //!!!!!Ajustar o arquivo txt pra incluir a mensalidade nesse construtor, talvez seja necessário mudar na classe Program também!!!!
-    public Cliente(String nome, int cpf, Date dataMatricula, Date vencimento, int id, Boolean statusMatricula,String senha) {
+    public Cliente(String tipoPlano,String nome, String cpf, Date dataMatricula, Date vencimento, int id, Boolean statusMatricula,double valorPlano){
+        contId++;
+        this.tipoPlano = tipoPlano;
         this.nome = nome;
         this.cpf = cpf;
         this.dataMatricula = dataMatricula;
         this.vencimento = vencimento;
         this.id = id;
         this.statusMatricula = statusMatricula;
-        this.senha = senha;
-        
+        this.valorPlano = valorPlano;
     }
 
+    public String getTipoPlano(){
+        return tipoPlano;
+    }
+    
     public int getId() {
         return id;
     }
-
-    public int getCpf() {
+    
+    public String getCpf() {
         return cpf;
-    }
-
-    public String getNome() {
-        return nome;
     }
 
     public String getDataMatricula() {
         return sdf.format(dataMatricula);
     }
-
-    public String getVencimento() {
-        return sdf.format(vencimento);
+    
+    public double getValorPlano() {
+        return valorPlano;
     }
-
-    public Boolean getStatusMatricula() {
-        return statusMatricula;
+    
+    public void setValorPlano(double valorPlano){
+        this.valorPlano = valorPlano;
     }
-
-    public double getMensalidade() {
-        return mensalidade;
+    
+    public String getNome() {
+        return nome;
     }
-
+    
     public void setNome(String nome) {
         this.nome = nome;
+    }
+    
+    public Boolean getStatusMatricula() {
+        return statusMatricula;
     }
 
     public void setStatusMatricula(Boolean statusMatricula) {
         this.statusMatricula = statusMatricula;
     }
-
-    public String getSenha() {
-        return senha;
+      
+    public String getVencimento() {
+        return sdf.format(vencimento);
     }
-
-    /*
-    public void setSenha(String senha) {
-        this.senha = senha;
+    
+    public void setVencimento(Date vencimento){//usado para atualizar as subclasses
+        this.vencimento = vencimento;
     }
-   */
     
-    
-    public void updateVencimento(){//atualiza pagamento
+    public void updateVencimento(){//atualiza data do pagamento
         Calendar cal = Calendar.getInstance();
         cal.setTime(dataMatricula);
-        cal.add(Calendar.MONTH, 1);
+        cal.add(Calendar.MONTH, acrescimo);
         vencimento = cal.getTime();
+    }  
+
+    public int getAcrescimo() {
+        return acrescimo;
+    }
+
+    public void setAcrescimo(int acrescimo) {
+        this.acrescimo = acrescimo;
     }
     
-    
-    
-    
-    //Fazer alguma verificação
-    public void updateSenha(String novaSenha){
-        
-    }
-    
-    public void writeFile(File file){//Testando como transferir os dados pro arquivo
-        String dados = nome + "," + cpf + "," + getDataMatricula() + "," + getVencimento() + "," + 
-               id + "," + statusMatricula + "," + senha;
+    public void writeFile(File file){
+        String dados = this.toString();
         String path = file.getAbsolutePath();
         try(BufferedWriter bw = new BufferedWriter(new FileWriter(path,true))){
             bw.write(dados);
@@ -141,7 +138,8 @@ public class Cliente {
     }
     
     @Override
-    public String toString(){//Facilitar visualização, colocar mais dados depois
-        return "Nome: " + nome + " Cpf: " + cpf + " Vencimento: " + vencimento;
+    public String toString(){
+        return tipoPlano + "," + nome + "," + cpf + "," + getDataMatricula() + "," + getVencimento() + "," + 
+               id + "," + statusMatricula + "," + this.valorPlano ;
     }
 }
