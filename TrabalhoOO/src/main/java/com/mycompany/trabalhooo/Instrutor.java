17446a619faba9a161b7724033a4fa46b7d88017
Pedro Henrique <pedroalmeidavrb@gmail.com>
2021-03-07 18:31:05 -0300

    Criando a classe Instrutor

---
+++ TrabalhoOO/src/main/java/com/mycompany/trabalhooo/Instrutor.java
@@ -0,0 +1,32 @@
+/*Deiverson Mourão Alves Pedroso (201965123A) 
+Deyvison Gregório Dias   (201835017)
+Pedro Henrique Almeida Cardoso Reis (201835039)
+Yuri de Oliveira (201835010)*/
+package com.mycompany.trabalhooo;
+import java.util.Scanner;
+
+/**
+ *
+ * @author pedro
+ */
+public class Instrutor {
+    
+    public Instrutor()
+    {
+       
+    }
+    
+    public void criarFicha()
+    {
+        Instrutor instrutor = new Instrutor();
+        
+        instrutor.criarFicha();
+    }
+    
+    /*public String nomeInstrutor(String nome) {
+        Scanner teclado = new Scanner(System.in);
+        nome = teclado.next();
+        
+        return nome;
+    }*/
+}
