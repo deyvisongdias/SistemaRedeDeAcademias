f664d4b67cd2fe7e5f8da78d71b5830c3e512b96
Yuri-1525 <63771905+Yuri-1525@users.noreply.github.com>
2021-03-07 21:23:55 -0300

    Update ClienteAnual.java

--- TrabalhoOO/src/main/java/com/mycompany/trabalhooo/ClienteAnual.java
+++ TrabalhoOO/src/main/java/com/mycompany/trabalhooo/ClienteAnual.java
@@ -4,18 +4,18 @@ import java.util.Date;
 
 public class ClienteAnual extends Cliente{
 
-    public ClienteAnual(String tipoPlano, String nome, String cpf, Date dataMatricula,String telefone) {
-        super(tipoPlano, nome, cpf, dataMatricula, telefone);
-        this.setDuracaoPlano(12);
+    public ClienteAnual(String tipoPlano, String nome, String cpf, Date dataMatricula,String telefone,String sexo){
+        super(tipoPlano, nome, cpf, dataMatricula, telefone,sexo);
         this.setDesconto(15);
         this.updateVencimento();
+        this.setNumeroParcelas(12);
     }
     
     //Cliente já cadastrado
-    public ClienteAnual(String tipoPlano, String nome, String cpf, Date dataMatricula, Date vencimento, int id, Boolean statusMatricula,
-            double mensalidade,String telefone) {
-        super(tipoPlano, nome, cpf, dataMatricula, vencimento, id, statusMatricula, mensalidade,telefone);
-        this.setDuracaoPlano(12);
+    public ClienteAnual(String tipoPlano, String nome, String cpf, Date dataMatricula, Date vencimento, int id,
+            Boolean statusMatricula, double mensalidade,String telefone,String sexo) {
+        super(tipoPlano, nome, cpf, dataMatricula, vencimento, id, statusMatricula, mensalidade,telefone,sexo);
+       this.setNumeroParcelas(12);
         this.setDesconto(15);
       
     }
